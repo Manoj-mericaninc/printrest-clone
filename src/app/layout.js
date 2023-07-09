3530d5a93f22903d02bf0a76e66cc7f0d242b2fd
manojkumar021997 <manojkumar020m@gmail.com>
2023-07-09 20:54:12 +0530

    navbar completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
+"use client"
+
 import Header from '../../components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Provider from './provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Provider>
         <Header/>
-        {children}</body>
+        {children}
+        </Provider>
+        </body>
     </html>
   )
 }
